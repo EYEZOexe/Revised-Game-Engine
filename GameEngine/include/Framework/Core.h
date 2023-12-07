3a8aa2a82879ed35d10980c833a7e0cbebf06f16
Tomas Tzorta UNI <greekharvarddev@gmail.com>
2023-12-07 03:10:09 +0000

    defining common datatypes

--- GameEngine/include/Framework/Core.h
+++ GameEngine/include/Framework/Core.h
@@ -6,9 +6,32 @@
 #define CORE_H
 
 #include <stdio.h>
+#include <memory>
+#include <vector>
+#include <map>
+#include <unordered_map>
 
 namespace Framework
 {
+    template<typename T>
+    using Uptr = std::unique_ptr<T>;
+
+    template<typename T>
+    using Sptr = std::shared_ptr<T>;
+
+    template<typename T>
+    using Wptr = std::weak_ptr<T>;
+
+    template<typename T>
+    using Vec = std::vector<T>;
+
+    template<typename a_keyType, typename a_valueType, typename a_predicate = std::less<a_keyType>>
+    using Map = std::map<a_keyType, a_valueType, a_predicate>; // a map is slower but it's ordered.
+
+    template<typename a_keyType, typename a_valueType, typename a_hasher = std::hash<a_keyType>>
+    using UMap = std::unordered_map<a_keyType, a_valueType, a_hasher>; // a hash map or dictionary is faster than a map but it's unordered.
+
+
 #define GE_LOG(x, ...) printf(x "\n", ##__VA_ARGS__) // This is a macro, the 2 ## are for when there are no arguments, it will still compile.
 }
 
